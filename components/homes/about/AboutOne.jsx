435112c41ca2f0c7655e00a1683bc9402cb83925
Arnaud Quatrevaux <arnaudqpro@outlook.com>
2024-06-16 01:47:57 +0200

    changes About page

--- components/homes/about/AboutOne.jsx
+++ components/homes/about/AboutOne.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { clientLogos } from "@/data/clientLogos";
 import { Navigation, Pagination, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { items } from "@/data/jobFeatures";
@@ -26,7 +25,7 @@ export default function AboutOne() {
 
         <div className="section-wrapper pl-60 pr-60">
           <div className="bostami-section-title-wrap mb-30">
-            <h3 className="section-title">What I do!</h3>
+            <h3 className="section-title">Skills</h3>
           </div>
 
           <div className="bostami-what-do-wrap mb-30">
@@ -50,60 +49,6 @@ export default function AboutOne() {
                   </div>
                 </div>
               ))}
-            </div>
-          </div>
-        </div>
-
-        <div className="section-wrapper bg-light-white-2 pt-45 pb-60 pl-60 pr-60">
-          <div className="bostami-section-title-wrap text-center mb-50">
-            <h3 className="section-title">clinet</h3>
-          </div>
-
-          <div className="bostami-client-slider">
-            <div className="swiper-container client_slide_active">
-              {showSlider && (
-                <Swiper
-                  // {...setting}
-                  modules={[Navigation, Pagination, Autoplay]}
-                  // loop={true}
-                  spaceBetween={30}
-                  slidesPerView={2}
-                  loop={true}
-                  autoplay={{
-                    delay: 3000, // Time between each slide (in milliseconds)
-                    disableOnInteraction: false, // Set to false if you want the auto slider to continue even when the user interacts with the slider (e.g., clicking on a slide).
-                  }}
-                  breakpoints={{
-                    // when window width is >= 576px
-                    450: {
-                      slidesPerView: 3,
-                    },
-                    // when window width is >= 768px
-                    768: {
-                      slidesPerView: 4,
-                    },
-                    1200: {
-                      // when window width is >= 992px
-                      slidesPerView: 5,
-                    },
-                  }}
-                >
-                  {clientLogos.map((elm, i) => (
-                    <SwiperSlide key={i}>
-                      <div className="swiper-slide">
-                        <Image
-                          height={62}
-                          width={264}
-                          style={{ height:'62px',width:'fit-content'}}
-                          className="bostami-client-slider-logo"
-                          src={elm.imgSrc}
-                          alt="client"
-                        />
-                      </div>
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
-              )}
             </div>
           </div>
         </div>
--- data/bioData.js
+++ data/bioData.js
@@ -1,8 +1,6 @@
 export const bioData = {
-  descOne: `I'm Creative Director and UI/UX Designer
-    from Sydney, Australia, working in web development and print media. I enjoy
+  descOne: `I'm Web Integrator from Saint Paul de Jarrat, France. I enjoy
     turning complex problems into simple, beautiful and intuitive designs.`,
   descTwo: `My aim is to bring across your message and
-    identity in the most creative way. I created web design for many famous brand
-    companies.`,
+    identity in the most creative way.`,
 };
