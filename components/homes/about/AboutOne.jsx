6734b69079c4810d3c62492f2bb6bf0b543d106c
Arnaud Quatrevaux <arnaudqpro@outlook.com>
2024-06-16 01:50:14 +0200

    push

--- components/homes/about/AboutOne.jsx
+++ components/homes/about/AboutOne.jsx
@@ -1,9 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Navigation, Pagination, Autoplay } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { items } from "@/data/jobFeatures";
 import { bioData } from "@/data/bioData";
 import Image from "next/image";
 
@@ -20,36 +17,6 @@ export default function AboutOne() {
             <h2 className="page-title">about</h2>
             <p>{bioData.descOne}</p>
             <p>{bioData.descTwo}</p>
-          </div>
-        </div>
-
-        <div className="section-wrapper pl-60 pr-60">
-          <div className="bostami-section-title-wrap mb-30">
-            <h3 className="section-title">Skills</h3>
-          </div>
-
-          <div className="bostami-what-do-wrap mb-30">
-            <div className="row">
-              {items.slice(0, 4).map((elm, i) => (
-                <div key={i} className="col-xxl-6 col-xl-6 col-lg-6">
-                  <div className={`bostami-what-do-item ${elm.bg} `}>
-                    <div className="icon" style={{ margin: "0 auto" }}>
-                      <Image
-                        height={40}
-                        width={130}
-                        style={{ margin: "0 auto" }}
-                        src={elm.icon}
-                        alt="feature"
-                      />
-                    </div>
-                    <div className="text">
-                      <h4 className="title">{elm.title}</h4>
-                      <p>{elm.description}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
           </div>
         </div>
 
