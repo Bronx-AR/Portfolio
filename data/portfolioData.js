e90d62f2b169de9489dc193e0a51b2db062eb5e7
Arnaud Quatrevaux <arnaudqpro@outlook.com>
2024-06-17 23:35:41 +0200

    changes Works page

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -3,198 +3,179 @@ export const portfolioData = [
     id: 1,
     class: "isotop-item logo",
     bgClass: "bg-prink",
-    imgSrc: "/assets/img/work/work-img-1.png",
-    subtitle: "Travel Landing , UX/UI",
-    title: "Chul urina",
+    imgSrc: "/assets/img/work/Auberge-la-Canebiere.jpg",
+    subtitle: "OpenClassrooms web integrator training project.",
+    title: "Booki",
     target: "#portfolio-1",
-    category: ["Logo"],
+    category: ["HTML/CSS"],
     project: "website",
-    languages: ["Photoshop", "Figma"],
-    client: "Envato",
-    previewLink: "#",
-    previewName: "www.envato.com",
+    languages: ["HTML", "CSS", "Figma"],
+    client: "Booki",
+    previewLink: "https://github.com/Bronx-AR/Booki",
+    previewName: "www.booki.com",
     desc: [
-      `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum conseqr quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, enim corporis omnis doloremque soluta inventore
-      dolorum consequuntur quo obcaecati rerum sit non.`,
-
-      ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consetur quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consequuntur.`,
+      `The company wanted to develop a website that allows its users to find accomodation and activities in the city of their choice.
+      For the project I intefrated the site interface in HTML and CSS, I had Figma models for mobile, tablet and desktop, as well as images and a summary
+      note on the specifies of the project. This allowed me to develop essential skills in responsive design, integrate a site according to a model as well
+      as install a front-end development environment.`,
     ],
   },
   {
     id: 2,
     class: "isotop-item design mockup",
     bgClass: "bg-catkrill",
-    imgSrc: "/assets/img/work/work-img-2.png",
-    subtitle: "Travel Landing , UX/UI",
-    title: "Aura Dione",
+    imgSrc: "/assets/img/work/jay-wennington-N_Y88TWmGwA-unsplash.jpg",
+    subtitle: "OpenClassroom web integrator training project.",
+    title: "OhMyFood",
     target: "#portfolio-1",
-    category: ["Mockup", "Graphic Design"],
+    category: ["HTML/CSS", "SASS"],
     project: "website",
-    languages: ["Photoshop", "Figma"],
+    languages: ["HTML", "SASS"],
     client: "Envato",
-    previewLink: "#",
-    previewName: "www.envato.com",
+    previewLink: "https://github.com/Bronx-AR/Ohmyfood",
+    previewName: "www.ohmyfood.com",
     desc: [
-      `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum conseqr quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, enim corporis omnis doloremque soluta inventore
-      dolorum consequuntur quo obcaecati rerum sit non.`,
-
-      ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consetur quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consequuntur.`,
+      `For this project, the client wanted a website that lists the menus of gourmet restaurants, the mission was to integrate the site in mobile-first, 
+      as well as to make the tablet and desktop version in accordance with the Figma models which were provided, while using SASS and including animations 
+      to enrich the user experience of the site, all by versioning the site on GitHub. This allowed me to develop skills in mobile-first responsive design, 
+      use of SASS as well as the use of GitHub.`,
     ],
   },
   {
     id: 3,
     class: "isotop-item logo mockup",
     bgClass: "bg-catkrill",
-    imgSrc: "/assets/img/work/work-img-3.png",
-    subtitle: "Travel Landing , UX/UI",
-    title: "T-Shirt Design",
+    imgSrc: "/assets/img/work/colors.jpg",
+    subtitle: "OpenClassroom web integrator training project.",
+    title: "Print-It",
     target: "#portfolio-1",
-    category: ["Mockup", "Logo"],
+    category: ["JavaScript"],
     project: "website",
-    languages: ["Photoshop", "Figma"],
-    client: "Envato",
-    previewLink: "#",
-    previewName: "www.envato.com",
+    languages: ["HTML", "JavaScript"],
+    client: "Print-It",
+    previewLink: "https://github.com/Bronx-AR/Print-it",
+    previewName: "www.print-it.com",
     desc: [
-      `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum conseqr quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, enim corporis omnis doloremque soluta inventore
-      dolorum consequuntur quo obcaecati rerum sit non.`,
-
-      ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consetur quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consequuntur.`,
+      `For this project the client, Print-in which is a printing company, wanted to dynamize their static
+      website by adding an interactive carousel. So my mission was to update the existing HTML code and add a carousel in JavaScript.
+      This project allowed me to work on the fundamentals of JavaScript.`,
     ],
   },
   {
     id: 4,
     class: "isotop-item mockup",
     bgClass: "bg-prink",
-    imgSrc: "/assets/img/work/work-img-4.png",
-    subtitle: "Travel Landing , UX/UI",
-    title: "Packaging Box",
+    imgSrc: "/assets/img/work/sophie-bluel.png",
+    subtitle: "OpenClassroom web integrator training project.",
+    title: "Sophie Bluel",
     target: "#portfolio-1",
-    category: ["Mockup"],
-    project: "website",
-    languages: ["Photoshop", "Figma"],
-    client: "Envato",
-    previewLink: "#",
-    previewName: "www.envato.com",
+    category: ["JavaScript"],
+    project: "portfolio",
+    languages: ["HTML", "SASS", "JavaScript", "API"],
+    client: "Sophie Bluel",
+    previewLink: "https://github.com/Bronx-AR/Sophie-Bluel",
+    previewName: "www.sophiebluel.com",
     desc: [
-      `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum conseqr quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, enim corporis omnis doloremque soluta inventore
-      dolorum consequuntur quo obcaecati rerum sit non.`,
-
-      ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consetur quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consequuntur.`,
+      `For this portfolio site of an interior designer: Sophie Bluel,
+      the mission was to integrate the dynamic part of the site, such as the creation of a gallery,
+      the creation of a user login page as well as the possibility of managing images (add or delete),
+      using javascript language and an API. This project allowed me to strengthen my knowledge of JavaScript
+      as well as learn to communicate with a database.
+      `,
     ],
   },
   {
     id: 5,
     class: "isotop-item design logo",
     bgClass: "bg-prink",
-    imgSrc: "/assets/img/work/work-img-5.png",
-    subtitle: "Travel Landing , UX/UI",
-    title: "Modern Bag Design",
+    imgSrc: "/assets/img/work/Kasa 1654513af767e6d7b860.jpg",
+    subtitle: "OpenClassroom web integrator training project.",
+    title: "Kasa",
     target: "#portfolio-1",
-    category: ["Logo", "Graphic Design"],
+    category: ["React",],
     project: "website",
-    languages: ["Photoshop", "Figma"],
-    client: "Envato",
-    previewLink: "#",
-    previewName: "www.envato.com",
+    languages: ["HTML", "SASS", "React"],
+    client: "Kasa",
+    previewLink: "https://github.com/Bronx-AR/Kasa",
+    previewName: "www.kasa.com",
     desc: [
-      `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum conseqr quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, enim corporis omnis doloremque soluta inventore
-      dolorum consequuntur quo obcaecati rerum sit non.`,
-
-      ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consetur quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consequuntur.`,
+      `For this project, the company wanted a entire redesign of their website and implement the
+      front-end of the application using React, Create-React-App, React Router and Sass;they also
+      wanted to add animations to improve the user experience. This project allowed me to learn
+      programming with React and therefore to develop my code into components.`,
     ],
   },
   {
     id: 6,
     class: "isotop-item mockup design",
     bgClass: "bg-catkrill",
-    imgSrc: "/assets/img/work/work-img-6.png",
-    subtitle: "Travel Landing , UX/UI",
-    title: "Chul urina",
+    imgSrc: "/assets/img/work/nina.webp",
+    subtitle: "OpenClassroom web integrator training project.",
+    title: "Nina Carducci",
     target: "#portfolio-1",
-    category: ["Mockup", "Graphic Design"],
-    project: "website",
-    languages: ["Photoshop", "Figma"],
-    client: "Envato",
-    previewLink: "#",
-    previewName: "www.envato.com",
+    category: ["SEO"],
+    project: "optimization",
+    languages: ["SEO"],
+    client: "Nina Carducci",
+    previewLink: "https://github.com/Bronx-AR/Nina-Carducci",
+    previewName: "www.ninacarducci.com",
     desc: [
-      `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum conseqr quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, enim corporis omnis doloremque soluta inventore
-      dolorum consequuntur quo obcaecati rerum sit non.`,
-
-      ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consetur quo
-      obcaecati rerum sit non. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Fuga consequatur delectus porro sapiente molestias,
-      magni quasi sed, sit amet consectetur adipisicing elit. Fuga
-      consequatur delectus porro sapiente molestias, magni quasi sed, enim
-      corporis omnis doloremque soluta inventore dolorum consequuntur.`,
+      `For this project it was necessary to make a global optimization of the site of
+      the photographer Nina Carducci, both in terms of performance and SEO, set up local
+      referencing with schema.org, add metas for social networks and make the necessary
+      modifications for the accessibility of the site. As well as providing an optimization
+      report to the client so that they can see the different changes made and the impact it
+      had on the site. So I learned to use the lighthouse tool, as well as the use of metas
+      with schema.org. It also allowed me to strengthen my knowledge of HTML semantics for
+      accessibility, as well as how to optimize images for a site.`,
+    ],
+  },
+  {
+    id: 7,
+    class: "isotop-item mockup design",
+    bgClass: "bg-catkrill",
+    imgSrc: "/assets/img/work/hall-expo.png",
+    subtitle: "OpenClassroom web integrator training project.",
+    title: "724 Events",
+    target: "#portfolio-1",
+    category: ["Debug"],
+    project: "debuging",
+    languages: ["JavaScript"],
+    client: "724 Events",
+    previewLink: "#",
+    previewName: "www.724events.com",
+    desc: [
+      `For this project, the events agency 724 Event wanted to correct a
+      few bugs on their site, particularly for the carousel as well as the
+      display of the correct months and images for each of their events.
+      To do this I learned to use the React developer tool as well as understand testing.
+      It also taught me how to write unit tests for different components, and strengthen
+      my knowledge of using the console in the middle of development.`,
+    ],
+  },
+  {
+    id: 8,
+    class: "isotop-item mockup design",
+    bgClass: "bg-catkrill",
+    imgSrc: "/assets/img/work/Arbre bancaire.webp",
+    subtitle: "OpenClassroom web integrator training project.",
+    title: "Argent Bank",
+    target: "#portfolio-1",
+    category: ["React", "API"],
+    project: "website",
+    languages: ["React", "Redux", "API"],
+    client: "Argent Bank",
+    previewLink: "https://github.com/Bronx-AR/ArgentBank",
+    previewName: "www.argentbank.com",
+    desc: [
+      `For this project Argent Bank wanted to integrate their entire site using React and Redux
+      as well as using green code best practices. They wanted the user to have access to their
+      different accounts when login, and to have the possibility of changing the username on it
+      (but not the possibility of changing the first or last name). Additionally they wanted a
+      document with possible API routes that could be created to retrieves all transactions by
+      accounts, given that they were in the creation phase. This project allowed me to strengthen
+      my knowledge in the use of React, and to learn how to use Redux to set up a global state on a
+      site, as well as to learn how to create API routes in yaml format.`,
     ],
   },
 ];
@@ -207,14 +188,26 @@ export const filterButtons = [
   },
   {
     id: 2,
-    text: "Mockup",
+    text: "HTML/CSS",
   },
   {
     id: 3,
-    text: "Graphic Design",
+    text: "SASS",
   },
   {
     id: 4,
-    text: "Logo",
+    text: "JavaScript",
+  },
+  {
+    id: 5,
+    text: "React",
+  },
+  {
+    id: 6,
+    text: "SEO"
+  },
+  {
+    id: 7,
+    text: "Debug"
   },
 ];
