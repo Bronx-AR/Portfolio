d6480b5cd00347af7b460c78c80889fd378d2424
Arnaud Quatrevaux <arnaudqpro@outlook.com>
2024-06-25 01:35:22 +0200

    changes Social Media

--- data/socials.js
+++ data/socials.js
@@ -2,19 +2,19 @@ export const socialMediaData = [
   {
     id: 1,
     className: "fa-brands fa-twitter",
-    href: "#",
+    href: "https://x.com/ArnaudQ09",
     color: "#1C9CEA",
   },
   {
     id: 2,
     className: "fa-brands fa-instagram",
-    href: "#",
+    href: "https://www.instagram.com/arnaudqtx/",
     color: "#e12a72",
   },
   {
     id: 3,
     className: "fa-brands fa-linkedin-in",
-    href: "#",
+    href: "https://www.linkedin.com/arnaud-quatrevaux-8478891b1/",
     color: "#144679",
   },
 ];
